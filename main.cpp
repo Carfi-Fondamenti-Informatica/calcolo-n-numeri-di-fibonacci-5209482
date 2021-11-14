6da9e0612593be2f8eb7b430e37db1574fce7e01
5209482 <91668718+5209482@users.noreply.github.com>
2021-11-14 12:50:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
-}
+    int n=0;
+    cin>>n;
+    if (n>=2){
+        int risultato, primo=1, secondo=0;
+        for(int i=1;i<=n;i++){
+            risultato=primo+secondo;
+            primo=secondo;
+            secondo=risultato;
+            cout<<risultato<<endl;
+        }
+    }else{
+        cout<<"errore"<<endl;
+    }
+    return 0;
